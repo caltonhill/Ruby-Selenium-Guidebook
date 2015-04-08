210240bf57db77f68db58d3211729eeac3968e24
caltonhill <macdonald.james@gmail.com>
2015-04-08 23:11:35 +0100

    Latest structure changes
    Restructured directories. Next section is "Organizing Your Test Runs"
    NB. Saucelabs active tunnel would not work. Seems to be difference in
    current version of Sauce gem 'selenium.rb' file. Unknown methods?

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@
 source "https://rubygems.org"
 
 # gem "rails"
+
+gem 'rspec', '~> 2.14.0'
+gem 'selenium-webdriver'
+gem 'sauce_whisk'
+gem 'sauce-connect'
--- config/cloud.rb
+++ config/cloud.rb
@@ -7,4 +7,11 @@ ENV['browser']          ||= 'internet_explorer'
 ENV['browser_version']  ||= '8'
 ENV['SAUCE_USERNAME']   = 'macdonaldjames'
 ENV['SAUCE_ACCESS_KEY'] = '9124401a-9b6c-4e50-9022-c6d27468c380'
+ENV['tunnel']           ||=''
+
+unless ENV['tunnel'].empty?
+  require 'sauce'
+  Sauce::Utilities::Connect.start
+  ENV['base_url']       = 'http://the-internet-local:4567'
+end
 
